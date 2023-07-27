1e76939cf5004031676337242b3eb34c7a826d42
unknown <jonikarell1@gmail.com>
2023-07-27 12:05:49 +0700

    edit margin features

--- components/Features.js
+++ components/Features.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { motion } from "framer-motion"
 import ScrolledAnimation from './ScrolledAnimation'
 import getScrolledAnimation from '@/utils/getScrolledAnimation'
+import featuresImage from '../public/assets/Illustration-2.png'
 
 const features = [
     "Powerfull online protection.",
@@ -21,15 +22,15 @@ const Features = () => {
                     <div className='grid grid-cols-1 lg:grid-cols-2 py-6 sm:py-10 gap-5 '>
                         <ScrolledAnimation>
                             <motion.div variants={scrollAnimation} className=' w-full flex justify-center  '>
-                                <Image src={'/assets/Illustration 2.png'} width={508.12} height={414.71} className='w-full' />
+                                <Image src={featuresImage} width={508.12} height={414.71} className='w-full' />
                             </motion.div>
 
                         </ScrolledAnimation>
                         <ScrolledAnimation>
-                            <motion.div className=' flex flex-col items-start justify-center  w-full lg:w-9/12 '>
+                            <motion.div className=' flex flex-col items-start ml-auto justify-center  w-full lg:w-9/12 '>
                                 <h1 className='text-4xl font-medium pt-12 mb-5'>We Provide Many Features You Can Use</h1>
                                 <p className='text-dark-500 leading-8 mb-5'>You can explore the features that we provide with fun and have their own functions each feature.</p>
-                                <ul className='flex flex-col gap-5 items-start  text-dark-500'>
+                                <ul className='flex flex-col gap-5  text-dark-500'>
                                     {features.map((feature, index) => (
                                         <motion.li
                                             variants={scrollAnimation}
